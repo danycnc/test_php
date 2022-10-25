455824e3993da66796bc0394ffd4c3571fc0e3b5
Daniele <97443760+danycnc@users.noreply.github.com>
2022-10-25 17:16:07 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=contacts',
-    'username' => 'daniele',
-    'password' => '60659599',
+    'username' => 'root',
+    'password' => 'password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
