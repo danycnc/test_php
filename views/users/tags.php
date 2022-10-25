6310005dfba9dc162c55c6a745662659d8229548
Daniele <97443760+danycnc@users.noreply.github.com>
2022-10-25 19:52:20 +0200

    Update tags.php

--- views/users/tags.php
+++ views/users/tags.php
@@ -5,7 +5,7 @@
 ?>
 <h1>Tag più usati</h1>
 
-<!-- sono arrivato a questa soluzione 'estrema' perchè dopo svariate prove, non riuscivo a stampare a video il count associato a ogni Tags. Tramite una var_dump ho scoperto che 'count' era effettivamente all'interno dell'array ritornato dal controller. E allora vai di metodo split come su JS.
+<!-- sono arrivato a questa soluzione 'estrema' perchè dopo svariate prove, non riuscivo a stampare a video il count associato a ogni Tags. Tramite una var_dump ho scoperto che 'count' era effettivamente all'interno dell'array ritornato dal controller.
 Mi rendo conto che la soluzione usata non è delle migliori (anzi...) e che mi sarò perso probabilmente in un bicchiere d'acqua, ma abbiate pietà (: -->
 
 <?php foreach ($tags as $tag) : ?>
