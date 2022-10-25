680bc00d9f193dcd3ecf993f6f0e0956060ec7eb
Daniele <97443760+danycnc@users.noreply.github.com>
2022-10-25 17:03:00 +0200

    Add files via upload

---
+++ contact_db.sql
@@ -0,0 +1,60 @@
+
+CREATE TABLE `users` (
+  `Nome` varchar(100) NOT NULL,
+  `Cognome` varchar(100) NOT NULL,
+  `Numero` varchar(100) NOT NULL,
+  `Tags` varchar(100) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+
+INSERT INTO `users` VALUES ('Haroun','Beresfore','480-330-9369','Project Manager',1);
+INSERT INTO `users` VALUES ('Frasier','Loweth','939-628-0639','Architect',2);
+INSERT INTO `users` VALUES ('Sapphire','MacCarter','511-608-8223','Construction Expeditor',3);
+INSERT INTO `users` VALUES ('Bone','Hateley','191-465-0610','Supervisor',4);
+INSERT INTO `users` VALUES ('Harriot','Nehlsen','516-453-5764','Electrician',5);
+INSERT INTO `users` VALUES ('Sadella','Heberden','173-841-3502','Construction Manager',6);
+INSERT INTO `users` VALUES ('Wilden','Meadowcroft','143-689-5874','Construction Worker',7);
+INSERT INTO `users` VALUES ('Emmy','Burress','222-821-6886','Engineer',8);
+INSERT INTO `users` VALUES ('Aldridge','Balharrie','964-848-3061','Surveyor',9);
+INSERT INTO `users` VALUES ('Kelley','Banbrook','145-299-0961','Construction Foreman',10);
+INSERT INTO `users` VALUES ('Sandye','Grand','103-486-4269','Electrician',11);
+INSERT INTO `users` VALUES ('Manya','Hair','805-248-6355','Estimator',12);
+INSERT INTO `users` VALUES ('Hyacinthia','Hedgeley','873-644-0516','Construction Expeditor',13);
+INSERT INTO `users` VALUES ('Roshelle','Melliard','326-765-7491','Construction Foreman',14);
+INSERT INTO `users` VALUES ('Corny','Clawson','235-772-8952','Subcontractor',15);
+INSERT INTO `users` VALUES ('Ulises','Creevy','669-771-0739','Subcontractor',16);
+INSERT INTO `users` VALUES ('Othella','Middell','597-381-1468','Estimator',17);
+INSERT INTO `users` VALUES ('Carmine','Hopkynson','326-331-7479','Construction Expeditor',18);
+INSERT INTO `users` VALUES ('Gilda','Hutcheson','103-183-7657','Engineer',19);
+INSERT INTO `users` VALUES ('Lindsey','Melby','858-649-8365','Construction Foreman',20);
+INSERT INTO `users` VALUES ('Guenna','Elion','857-631-0839','Electrician',21);
+INSERT INTO `users` VALUES ('Abramo','Assur','280-204-6390','Construction Foreman',22);
+INSERT INTO `users` VALUES ('Debby','Dowry','163-320-8668','Project Manager',23);
+INSERT INTO `users` VALUES ('Violette','Bateson','973-249-4155','Supervisor',24);
+INSERT INTO `users` VALUES ('Xenia','Aizic','869-319-2656','Subcontractor',25);
+INSERT INTO `users` VALUES ('Jenny','Million','938-453-1340','Estimator',26);
+INSERT INTO `users` VALUES ('Helsa','Syres','594-821-9862','Subcontractor',27);
+INSERT INTO `users` VALUES ('Saxe','Benedek','933-213-1964','Construction Expeditor',28);
+INSERT INTO `users` VALUES ('Sileas','Woollaston','279-443-0807','Construction Manager',29);
+INSERT INTO `users` VALUES ('Julianna','Fabler','505-746-5437','Electrician',30);
+INSERT INTO `users` VALUES ('Kennett','Iacomi','837-362-9041','Architect',31);
+INSERT INTO `users` VALUES ('Joeann','MacCurley','580-125-8324','Architect',32);
+INSERT INTO `users` VALUES ('Allie','Bloomfield','777-971-6368','Architect',33);
+INSERT INTO `users` VALUES ('Jane','Scyner','657-152-4319','Project Manager',34);
+INSERT INTO `users` VALUES ('Murray','Okenden','421-540-9377','Construction Foreman',35);
+INSERT INTO `users` VALUES ('Rutger','Suggey','233-950-3468','Architect',36);
+INSERT INTO `users` VALUES ('Olvan','Morfield','633-250-0829','Estimator',37);
+INSERT INTO `users` VALUES ('Heinrick','O Sullivan','233-641-7652','Subcontractor',38);
+INSERT INTO `users` VALUES ('Liane','Speere','564-378-7212','Estimator',39);
+INSERT INTO `users` VALUES ('Nicolas','Dalrymple','123-277-4697','Surveyor',40);
+INSERT INTO `users` VALUES ('Eilis','Pawel','327-300-5154','Subcontractor',41);
+INSERT INTO `users` VALUES ('Trudi','Gookes','383-160-4468','Electrician',42);
+INSERT INTO `users` VALUES ('Michele','Kelsey','693-518-0955','Subcontractor',43);
+INSERT INTO `users` VALUES ('Marga','Kersley','605-310-2199','Surveyor',44);
+INSERT INTO `users` VALUES ('Abie','Catterill','767-767-6769','Engineer',45);
+INSERT INTO `users` VALUES ('Baron','Gregolin','915-205-6206','Construction Expeditor',46);
+INSERT INTO `users` VALUES ('Della','Willingam','645-706-0224','Construction Worker',47);
+INSERT INTO `users` VALUES ('Dexter','Gronno','124-931-9387','Architect',48);
+INSERT INTO `users` VALUES ('Nadiya','Cage','753-321-3267','Project Manager',49);
+INSERT INTO `users` VALUES ('Cory','Bowling','913-966-1025','Engineer',50);
